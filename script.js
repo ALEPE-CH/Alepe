5aa1ee3f1149c69fd9c42617e993fa85de316f30
AlgoAdventurer <janis.kuhn@gmx.ch>
2024-11-19 17:17:43 +0100

    v1
    added the files to the main branche

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector(".hamburger");
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('active');
+    hamburger.classList.toggle("active");
+});
