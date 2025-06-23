5d447aa2b65e5792314ed8b0bf46d7e8733eaa82
dvnyms <danyjmsoares@gmail.com>
2025-06-23 20:44:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = navLinks.querySelectorAll("a");
+const nav = document.querySelector("nav");
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  hamburger.classList.toggle("active");
-});
-
-// Fermer menu mobile quand on clique sur un lien
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    if(navLinks.classList.contains("active")) {
-      navLinks.classList.remove("active");
-      hamburger.classList.remove("active");
-    }
-  });
+    nav.classList.toggle("active");
+    hamburger.classList.toggle("active");
 });
