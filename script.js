89abc10b1ae84ae84475f1049753d61febee6b5b
dvnyms <danyjmsoares@gmail.com>
2025-09-21 20:53:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Ajoute un scroll doux quand on clique sur les liens du menu
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  });
-});
+
