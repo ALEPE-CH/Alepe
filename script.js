35407f25ebb29646e2fbfd906870b7c9792e4e44
dvnyms <danyjmsoares@gmail.com>
2025-09-21 20:54:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('active');
+    hamburger.classList.toggle("active");
+});
 
