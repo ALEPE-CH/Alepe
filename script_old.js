465bbad7688a9c4fe4972a220c451d5909971c61
dvnyms <danyjmsoares@gmail.com>
2025-11-17 10:42:18 +0100

    Delete script_old.js

--- script_old.js
+++
@@ -1,5 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-document.querySelector('.hamburger').addEventListener('click', function () {
-    document.querySelector('nav').classList.toggle('active');
-    hamburger.classList.toggle("active");
-});
